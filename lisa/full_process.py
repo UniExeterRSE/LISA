a18dffce83900b690723d54674ffd13d32388f24
Tom Wilson <t.wilson6@exeter.ac.uk>
2025-01-10 15:54:53 +0000

    experiment: tuned just z

--- lisa/full_process.py
+++ lisa/full_process.py
@@ -4,6 +4,8 @@ from loguru import logger
 from tqdm import tqdm
 
 from lisa.config import MAIN_DATA_DIR, PROCESSED_DATA_DIR
+from lisa.dataset import process_files
+from lisa.features import feature_extraction
 from lisa.modeling.remote_multipredictor import main as multipredictor_main
 
 
@@ -11,7 +13,7 @@ def main(
     input_path: Path = MAIN_DATA_DIR,
     output_path: Path = PROCESSED_DATA_DIR / "reduced_main_data.parquet",
     models: list[str] = ["LGBM"],
-    run_id: str = "tuned",
+    run_id: str = "z_tuned",
     measures=["global angle", "mag", "gyro", "accel"],
     locations=["pelvis", "thigh", "shank", "foot_", "foot sensor"],
     dimensions=["y", "z"],
@@ -21,27 +23,27 @@ def main(
     Script for end-to-end processing of the LISA dataset.
     """
 
-    # missing_labels = {2: "thigh_l", 6: "pelvis", 7: "pelvis", 16: "thigh_l"}
+    missing_labels = {2: "thigh_l", 6: "pelvis", 7: "pelvis", 16: "thigh_l"}
     # skip_participants = list(range(1, 15))
-    # skip_participants = [15, 16]
+    skip_participants = [15, 16]
 
     window = 800
     split = 0.8
 
-    # feature_extraction(
-    #     process_files(
-    #         input_path,
-    #         skip_participants,
-    #         missing_labels,
-    #         measures,
-    #         locations,
-    #         dimensions,
-    #     ).collect(),
-    #     output_path,
-    #     window,
-    #     stats,
-    #     False,
-    # )
+    feature_extraction(
+        process_files(
+            input_path,
+            skip_participants,
+            missing_labels,
+            measures,
+            locations,
+            dimensions,
+        ).collect(),
+        output_path,
+        window,
+        stats,
+        False,
+    )
     logger.info("Completed processing")
 
     for model in tqdm(models):
