c67ce103411446e1fa775f695ad2e20fe30ab783
Tom Wilson <t.wilson6@exeter.ac.uk>
2025-01-23 13:26:02 +0000

    experiment: limited imus: foot, foot+pelvis

--- lisa/full_process.py
+++ lisa/full_process.py
@@ -4,16 +4,18 @@ from loguru import logger
 from tqdm import tqdm
 
 from lisa.config import MAIN_DATA_DIR, PROCESSED_DATA_DIR
+from lisa.dataset import process_files
+from lisa.features import feature_extraction
 from lisa.modeling.remote_multipredictor import main as multipredictor_main
 
 
 def main(
     input_path: Path = MAIN_DATA_DIR,
     output_path: Path = PROCESSED_DATA_DIR / "reduced_main_data.parquet",
-    models: list[str] = ["RF"],
-    run_id: str = "tuned",
+    models: list[str] = ["LGBM"],
+    run_id: str = "",
     measures=["global angle", "mag", "gyro", "accel"],
-    locations=["pelvis", "thigh", "shank", "foot_", "foot sensor"],
+    # locations=["pelvis", "thigh", "shank", "foot_", "foot sensor"],
     dimensions=["z"],
     stats=["min", "max"],
 ):
@@ -21,34 +23,37 @@ def main(
     Script for end-to-end processing of the LISA dataset.
     """
 
-    # missing_labels = {2: "thigh_l", 6: "pelvis", 7: "pelvis", 16: "thigh_l"}
-    # # skip_participants = list(range(1, 15))
-    # skip_participants = [15, 16]
+    missing_labels = {2: "thigh_l", 6: "pelvis", 7: "pelvis", 16: "thigh_l"}
+    # skip_participants = list(range(1, 15))
+    skip_participants = [15, 16]
 
     window = 800
     split = 0.8
 
-    # feature_extraction(
-    #     process_files(
-    #         input_path,
-    #         skip_participants,
-    #         missing_labels,
-    #         measures,
-    #         locations,
-    #         dimensions,
-    #     ).collect(),
-    #     output_path,
-    #     window,
-    #     stats,
-    #     False,
-    # )
-    logger.info("Completed processing")
+    for locations in [["foot_"], ["pelvis", "foot_"]]:
+        logger.info(f"experiment {locations}")
+        run_id = "+".join(locations)
+        feature_extraction(
+            process_files(
+                input_path,
+                skip_participants,
+                missing_labels,
+                measures,
+                locations,
+                dimensions,
+            ).collect(),
+            output_path,
+            window,
+            stats,
+            False,
+        )
+        logger.info("Completed processing")
 
-    for model in tqdm(models):
-        run_name = model + "_" + run_id
-        multipredictor_main(output_path, run_name, model, window, split, True)
+        for model in tqdm(models):
+            run_name = model + "_" + run_id
+            multipredictor_main(output_path, run_name, model, window, split, True)
 
-    logger.success("Completed training")
+        logger.success("Completed training")
 
 
 if __name__ == "__main__":
